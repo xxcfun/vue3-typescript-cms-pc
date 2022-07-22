e71941fc474090323ed5f89fac9c2953a0f94877
xxcfun <echo_dyc@163.com>
2022-07-22 16:03:29 +0800

    feat(cms): initialize project

---
+++ src/views/login/login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>login</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'login'
+})
+</script>
+
+<style lang="less"></style>
---
+++ src/views/main/main.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>main</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'main'
+})
+</script>
+
+<style lang="less"></style>
